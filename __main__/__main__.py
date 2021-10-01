cd32d2f6ecb93f711129e755201fe5739a626703
carly-west <carly.west99@gmail.com>
2021-10-01 15:31:45 -0600

    hello

--- __main__/__main__.py
+++ __main__/__main__.py
@@ -1,1 +1,11 @@
 print("Welcome to our collaborative program.")
+
+from west import display_carly
+from davenport import davenport
+from sanchez import display_rodrigo
+from whitmer import whitmer
+
+print(display_carly())
+print(davenport())
+print(display_rodrigo())
+print(whitmer())
